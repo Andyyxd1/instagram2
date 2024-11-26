045d452f828d3e01c79b4459df223138505bad69
Andyyxd1 <114660172+Andyyxd1@users.noreply.github.com>
2024-11-26 12:11:46 +0100

    index.js

--- instagram2/src/index.js
+++ instagram2/src/index.js
@@ -1,33 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import axios from 'axios';
-
-axios.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token'); // Replace with your token storage logic
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
-axios.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      console.error("Unauthorized - Redirecting to login");
-      // Redirect or handle unauthorized error globally
-    }
-    return Promise.reject(error);
-  }
-);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -36,7 +12,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
